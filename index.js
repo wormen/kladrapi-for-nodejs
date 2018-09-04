908feab4ecc79a760144f32cc78f895bfabba532
Oleg Bogdanov <olegbogdanov86@gmail.com>
2018-09-04 18:36:20 +0900

    no message

--- index.js
+++ index.js
@@ -7,90 +7,95 @@
 
 "use strict";
 
-const http = require("http");
-const util = require("util");
 const request = require('request');
-
 
 class Kladr {
 
-    constructor(opts){
+  constructor(opts) {
 
-        this.isFree = true;
-        this.token = null;
+    this.isFree = true;
+    this.token = null;
 
-        if(util.isObject(opts)){
-            if(opts.token){
-                this.isFree = false;
-                this.token = opts.token;
-            }
+    if (isObject(opts)) {
+      if (opts.token) {
+        this.isFree = false;
+        this.token = opts.token;
+      }
+    }
+
+  }
+
+  /**
+   * Формирование query string
+   * @param json
+   * @returns {*}
+   */
+  param(json) {
+    if (Object.keys(json).length === 0) {
+      return '';
+    } else {
+      return '?' +
+        Object.keys(json).map((key) => {
+          return encodeURIComponent(key) + '=' +
+            encodeURIComponent(json[key]);
+        }).join('&');
+    }
+  }
+
+  buildQuery(query) {
+
+    let queryString = {};
+
+    if (query.ParentType && query.ParentId) {
+      queryString[query.ParentType + 'Id'] = query.ParentId;
+    }
+
+    for (let key in query) {
+      if (key.includes('ParentType') || key.includes('ParentId')) {
+        continue;
+      }
+      queryString[key] = query[key];
+    }
+
+    // токен только для платной версии
+    if (!this.isFree) {
+      queryString.token = this.token;
+    }
+
+    return this.param(queryString)
+  }
+
+  getData(query, cb) {
+    if (!query) {
+      cb(new Error('Запрос не может быть пустым', 'emptyQuery'), null);
+    } else if (!isObject(query)) {
+      cb(new Error('Параметры должны иметь тип Object', 'notObject'), null);
+    } else if (!query.contentType) {
+      cb(new Error('Не указан обязательный параметр contentType', 'notСontentType'), null);
+    }
+
+    else {
+
+      let url = `http://${this.isFree ? 'kladr-api.ru' : 'kladr-api.com'}/api.php` + this.buildQuery(query);
+      let opts = {
+        method: 'GET',
+        url: url,
+        json: true
+      };
+
+      request(opts, (error, response, body) => {
+        if (typeof body === 'string') {
+          cb(new Error(body), null);
+        } else {
+          cb(error, body);
         }
-
+      });
     }
-
-    /**
-     * Формирование query string
-     * @param json
-     * @returns {*}
-     */
-    param(json){
-        if(Object.keys(json).length == 0) return '';
-        else
-            return '?' +
-                Object.keys(json).map(function(key) {
-                    return encodeURIComponent(key) + '=' +
-                        encodeURIComponent(json[key]);
-                }).join('&');
-    }
-
-    buildQuery(query){
-
-        var queryString = {};
-
-        if (query.ParentType && query.ParentId)
-            queryString[query.ParentType + 'Id'] = query.ParentId;
-
-        for(let key in query){
-            if(key.includes('ParentType') || key.includes('ParentId')) continue;
-            queryString[key] = query[key];
-        }
-
-        // токен только для платной версии
-        if(!this.isFree)
-            queryString.token = this.token;
-
-        return this.param(queryString)
-    }
-
-    getData(query, cb){
-        if (!query)
-            cb(new Error('Запрос не может быть пустым', 'emptyQuery'), null);
-
-        else if(!util.isObject(query))
-            cb(new Error('Параметры должны иметь тип Object', 'notObject'), null);
-
-        else if(!query.contentType)
-            cb(new Error('Не указан обязательный параметр contentType', 'notСontentType'), null);
-
-        else{
-
-            let url = `http://${this.isFree ? 'kladr-api.ru' : 'kladr-api.com'}/api.php` + this.buildQuery(query);
-            let opts = {
-                method: 'GET',
-                url: url,
-                json: true
-            };
-
-            request(opts, (error, response, body)=>{
-                if(util.isString(body))
-                    cb(new Error(body), null);
-                else
-                    cb(error, body);
-            });
-
-        }
-    }
-
+  }
 }
 
-module.exports = Kladr;
+module.exports = module.exports['default'] = Kladr;
+
+function isObject(arg) {
+  return arg !== null && typeof arg === 'object';
+}
